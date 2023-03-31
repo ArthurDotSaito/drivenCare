8ceca9eb3c7a144c9b71391810f285fd4edb43c9
Arthur Saito <arthur_saito1998@hotmail.com>
2023-03-31 00:45:30 -0300

    testing git

---
+++ src/routes/doctorsRoutes.js
@@ -0,0 +1,6 @@
+import { Router } from "express";
+
+const doctorRoutes = Router();
+
+doctorRoutes.use("/signIn");
+doctorRoutes.use("/signUp");
--- src/routes/patientsRoutes.js
+++ src/routes/patientsRoutes.js
@@ -1,2 +1,6 @@
 import { Router } from "express";
 
+const patientRoutes = Router();
+
+patientRoutes.use("/signIn");
+patientRoutes.use("/signUp");
