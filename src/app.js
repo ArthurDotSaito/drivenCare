e80029325f246ba05ed10a93bacc032c017acea0
Arthur Saito <arthur_saito1998@hotmail.com>
2023-03-31 00:36:53 -0300

    env: config. enviroment

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+import { Router } from 'express';
+import routes from './routes/index.js'
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+app.use(routes);
+
+app.listen(port, (error) =>{
+    if(error) return console.log("Unable to listen to server");
+    console.log(`server is running on port ${port}`);
+})
+
