4e1935e04839f2dc9887f1a10b25638a88148e9c
Arthur Saito <arthur_saito1998@hotmail.com>
2023-04-01 20:28:35 -0300

    feat: create Doctor OK. Tested

--- src/middlewares/validateSchema.js
+++ src/middlewares/validateSchema.js
@@ -4,10 +4,10 @@ export function validadeSchema(schema){
     return (req,res,next) =>{
         const { error } = schema.validate(req.body, { abortEarly: false });
         if(error){
+            console.log("deu ruim schema")
             const errors = error.details.map((detail) => detail.message);
             throw err.unprocessableEntity(errors);
         }
-        console.log("Schema OK")
         next();
     }
 }
